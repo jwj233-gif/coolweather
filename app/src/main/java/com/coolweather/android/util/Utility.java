9c183ef5f053d7225dbed141eb488a75c5fd8545
jack248392213 <jwj248392213@163.com>
2020-06-24 08:07:44 +0800

    提交

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,21 +5,20 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
-
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Utility {
-    /**
-     * 解析和处理服务器返回的省级数据
-     */
+
     public static boolean handleProvinceResponse(String response) {
         if (!TextUtils.isEmpty(response)) {
             try {
                 JSONArray allProvinces = new JSONArray(response);
-                for (int i = 0; i < allProvinces.length(); i++) {
+                for (int i = 0; i < allProvinces.length(); i++){
                     JSONObject provinceObject = allProvinces.getJSONObject(i);
                     Province province = new Province();
                     province.setProvinceName(provinceObject.getString("name"));
@@ -27,16 +26,13 @@ public class Utility {
                     province.save();
                 }
                 return true;
-            } catch (JSONException e) {
+            }catch (JSONException e) {
                 e.printStackTrace();
             }
         }
         return false;
     }
 
-    /**
-     * 解析和处理服务器返回的市级数据
-     */
     public static boolean handleCityResponse(String response, int provinceId) {
         if (!TextUtils.isEmpty(response)) {
             try {
@@ -50,16 +46,13 @@ public class Utility {
                     city.save();
                 }
                 return true;
-            } catch (JSONException e) {
+            }catch (JSONException e) {
                 e.printStackTrace();
             }
         }
         return false;
     }
 
-    /**
-     * 解析和处理服务器返回的县级数据
-     */
     public static boolean handleCountyResponse(String response, int cityId) {
         if (!TextUtils.isEmpty(response)) {
             try {
@@ -73,10 +66,22 @@ public class Utility {
                     county.save();
                 }
                 return true;
-            } catch (JSONException e) {
+            }catch (JSONException e) {
                 e.printStackTrace();
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
